a47ef4bb754fb98bf50e1ef80210d79aaa2a08f6
Ayumu Inaba <ainaba@microsoft.com>
2022-11-07 18:10:50 +0900

    dummy random

--- api/aspnetcore/FusionDev.Samples.TodoApi/Controllers/TodoControllerImpl.cs
+++ api/aspnetcore/FusionDev.Samples.TodoApi/Controllers/TodoControllerImpl.cs
@@ -21,10 +21,18 @@ namespace FusionDev.Samples.TodoApi.Controllers
 
         public Task<ICollection<TodoItem>> ListAllTodoItemAsync()
         {
+            var seed = new Random();
+            
             ICollection<TodoItem> ret = new List<TodoItem>() {
-                new TodoItem() { Id = 1, Title = "Task1", Status = "Active", Task = "経費を清算する", UpdatedOn = DateTimeOffset.Now.AddDays(-1), Duedate = DateTime.Now.AddDays(1) },
-                new TodoItem() { Id = 2, Title = "Task2", Status = "Done", Task = "出張を申請する", UpdatedOn = DateTimeOffset.Now.AddDays(-2), Duedate = DateTime.Now.AddDays(2) },
-                new TodoItem() { Id = 3, Title = "Task3", Status = "Active", Task = "体温を報告する", UpdatedOn = DateTimeOffset.Now.AddDays(-3), Duedate = DateTime.Now.AddDays(3) }
+                new TodoItem() { 
+                    Id = 1, Title = "Task1", Status = "Active", Task = "経費を清算する", 
+                    UpdatedOn = DateTimeOffset.Now.AddDays(-seed.Next(1, 5)), Duedate = DateTime.Now.AddDays(seed.Next(1, 5)) },
+                new TodoItem() { 
+                    Id = 2, Title = "Task2", Status = "Done", Task = "出張を申請する", 
+                    UpdatedOn = DateTimeOffset.Now.AddDays(-seed.Next(1, 5)), Duedate = DateTime.Now.AddDays(seed.Next(1, 5)) },
+                new TodoItem() { 
+                    Id = 3, Title = "Task3", Status = "Active", Task = "体温を報告する", 
+                    UpdatedOn = DateTimeOffset.Now.AddDays(-seed.Next(1, 5)), Duedate = DateTime.Now.AddDays(seed.Next(1, 5)) }
                 };
 
             return Task.FromResult(ret);
